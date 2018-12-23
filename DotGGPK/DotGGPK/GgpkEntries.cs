fb73670836acb5ad435408a3e12f21de4f4ac439
Tobias Koch <t.koch@tk-software.de>
2018-12-23 16:11:07 +0100

    Adding source code

---
+++ DotGGPK/DotGGPK/GgpkEntries.cs
@@ -0,0 +1,94 @@
+﻿#region MIT License
+// The MIT License (MIT)
+//
+// Copyright © 2018-2019 Tobias Koch <t.koch@tk-software.de>
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+#endregion
+
+namespace DotGGPK
+{
+    /// <summary>
+    /// Contains methods to read a ggpk archive in raw format.
+    /// </summary>
+    public static class GgpkEntries
+    {
+        #region Methods
+
+        /// <summary>
+        /// Reads the given ggpk archive file and returns all <see cref="GgpkEntry">entries</see>.
+        /// </summary>
+        /// <param name="fileName">The ggpk archive file.</param>
+        /// <returns>All <see cref="GgpkEntry">entries</see> read from the file.</returns>
+        /// <exception cref="ArgumentNullException"><c>fileName</c> is <c>null</c>.</exception>
+        /// <exception cref="FileNotFoundException"><c>fileName</c> does not exist.</exception>
+        public static IEnumerable<GgpkEntry> FromFile(string fileName)
+        {
+            if (fileName is null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            return FromFile(new FileInfo(fileName));
+        }
+
+        /// <summary>
+        /// Reads the given ggpk archive file and returns all <see cref="GgpkEntry">entries</see>.
+        /// </summary>
+        /// <param name="file">The ggpk archive file.</param>
+        /// <returns>All <see cref="GgpkEntry">entries</see> read from the file.</returns>
+        /// <exception cref="ArgumentNullException"><c>file</c> is <c>null</c>.</exception>
+        /// <exception cref="FileNotFoundException"><c>file</c> does not exist.</exception>
+        public static IEnumerable<GgpkEntry> FromFile(FileInfo file)
+        {
+            if (file is null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException($"Archive file {file.FullName} not found", file.FullName);
+            }
+
+            using (BinaryReader ggpkStreamReader = new BinaryReader(File.OpenRead(file.FullName)))
+            {
+                while (ggpkStreamReader.BaseStream.Position < ggpkStreamReader.BaseStream.Length)
+                {
+                    uint entryLength = ggpkStreamReader.ReadUInt32();
+                    string entryType = Encoding.ASCII.GetString(ggpkStreamReader.ReadBytes(4));
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
