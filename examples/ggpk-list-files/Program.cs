c7fe09afd9f0d0bf38cd95bd30478b77bcac443e
Tobias Koch <t.koch@tk-software.de>
2019-01-07 22:32:53 +0100

    Adding example

---
+++ examples/ggpk-list-files/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using DotGGPK;
+
+namespace ggpk_list_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GgpkArchive archive = GgpkArchive.From(Path.Combine(Environment.GetEnvironmentVariable("POE_PATH"), "content.ggpk"));
+            ListFiles(archive.Root);
+        }
+
+        static void ListFiles(IGgpkDirectory currentDirectory)
+        {
+            foreach (IGgpkFile file in currentDirectory.Files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+
+            foreach (IGgpkDirectory directory in currentDirectory.Directories)
+            {
+                ListFiles(directory);
+            }
+        }
+    }
+}
