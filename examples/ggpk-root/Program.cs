7b323eee316b879c38c0d797d048b1ef3a466a8a
Tobias Koch <t.koch@tk-software.de>
2019-01-12 22:12:13 +0100

    Adding additional example

---
+++ examples/ggpk-root/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using DotGGPK;
+
+namespace ggpk_root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GgpkArchive archive = GgpkArchive.From(Path.Combine(Environment.GetEnvironmentVariable("POE_PATH"), "content.ggpk"));
+            PrintDirectory(archive.Root);
+        }
+
+        static void PrintDirectory(IGgpkDirectory directory)
+        {
+            foreach (var file in directory.Files)
+            {
+                Console.WriteLine(file.FullName);
+            }
+
+            foreach (var subDirectory in directory.Directories)
+            {
+                PrintDirectory(subDirectory);
+            }
+        }
+
+    }
+}
